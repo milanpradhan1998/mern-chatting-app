deb7a6393c4b37037f66458fd0a82513b42a0ab9
milanpradhan1998 <milanpradhan1998@gmail.com>
2023-02-28 18:51:32 +0530

    first

---
+++ Express Server/app.js
@@ -0,0 +1,28 @@
+const { request, response } = require("express");
+const express = require("express");
+const dotenv = require("dotenv");
+const chats = require("./data/data");
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 7700;
+
+app.get("/", (request, response) => {
+  response.send("Express server is running on port :7700");
+});
+//CHAT DATA API
+app.get("/api/chat/", (request, response) => {
+  response.send(chats);
+});
+//SINGLE USER CHAT DATA USING ID.
+app.get("/api/chat/:id", (request, response) => {
+  let { id } = request.params;
+  console.log(id);
+  let singleChat = chats.find((c) => c._id === request.params.id);
+  console.log(singleChat);
+  response.send(singleChat);
+});
+
+app.listen(PORT, () => {
+  console.log("status:", true);
+  console.log(`Server is running on port:${PORT}`);
+});
